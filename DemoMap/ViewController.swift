2d553067107e7a120ace0d27f49e1f83eb58b367
Ольга Топанова <developerdroidot@gmail.com>
2019-07-23 10:45:50 +0300

    Initial Commit

---
+++ DemoMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoMap
+//
+//  Created by  Stanislav Topanov on 7/23/19.
+//  Copyright © 2019  Stanislav Topanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
